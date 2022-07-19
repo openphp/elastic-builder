4bb38929293c450c544721c7036e3f22629bdae8
wangzhiqiang <zhiqiangwang@aliyun.com>
2022-07-19 20:32:25 +0800

    swoole

--- tests/Connect.php
+++ tests/Connect.php
@@ -3,20 +3,53 @@
 namespace Openphp\ElasticBuilder\Tests;
 
 use Elasticsearch\ClientBuilder;
+use Openphp\ElasticBuilder\CoroutineHandler;
 use Openphp\ElasticBuilder\Elastic;
 
 class Connect
 {
     /**
+     * @var string[]
+     */
+    public static $hosts = [
+        "127.0.0.1:9200",
+    ];
+
+    /**
      * @return \Elasticsearch\Client
      */
     public static function client()
     {
-        return ClientBuilder::create()->setHosts([
-            "127.0.0.1:9200",
-        ])->setSSLVerification(false)->build();
+        return ClientBuilder::create()
+            ->setHosts(static::$hosts)
+            ->setSSLVerification(false)->build();
     }
 
+    /**
+     * @return \Elasticsearch\Client
+     */
+    public static function swooleClient()
+    {
+        return ClientBuilder::create()
+            ->setHandler(new CoroutineHandler())
+            ->setSSLVerification(false)
+            ->setHosts(static::$hosts)
+            ->build();
+    }
+
+    /**
+     * @return \Elasticsearch\Client
+     */
+    public function clients()
+    {
+        if (function_exists('swoole_version')
+            && class_exists('\Swoole\Coroutine')
+            && function_exists('\Swoole\Coroutine::getCid')
+            && \Swoole\Coroutine::getCid() > 0) {
+            return static::swooleClient();
+        }
+        return static::client();
+    }
 
     /**
      * @return Elastic|null
